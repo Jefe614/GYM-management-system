5b52fafb5d3c35d571564d4a0f12517d2951c67e
Geoffrey Kimani <94698428+Jefe614@users.noreply.github.com>
2024-02-08 17:20:47 +0300

    gmail handling

--- gymsystem/settings.py
+++ gymsystem/settings.py
@@ -144,9 +144,9 @@ LOGOUT_REDIRECT_URL = 'home'
 #email handling
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'www.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jeffkimani32@gmail.com'
-EMAIL_HOST_PASSWORD = 'jeffkimani32@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_gmail_password'
 DEFAULT_FROM_EMAIL = 'jeffkimani32@gmail.com'
